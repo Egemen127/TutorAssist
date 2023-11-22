a5047430d8c289040c0b8068b2fbdc04213d3b55
Egemen <egemendurmuss@gmail.com>
2023-11-21 21:25:26 -0500

    Added course filtering by course name, student name & tutor name

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,7 +2,7 @@ import './App.css';
 //import Modal from '@mui/material/Modal';
 //import Box from '@mui/material/Box';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
-import { Card,  Typography as Typo,Accordion,AccordionSummary, AccordionDetails} from '@mui/material';
+import {List, ListItem, Card,  Typography as Typo,Accordion,AccordionSummary, AccordionDetails} from '@mui/material';
 import { useLoaderData } from "react-router-dom";
 import * as React from 'react';
 import Utility from './Utility';
@@ -15,7 +15,7 @@ function Dashboard(props) {
         const effect = async ()=>{
         console.log("use effect triggered");
         //getting the tutors
-        const res = await Utility.TutorGetTutors().then(res=>{setTutors(res.data); return res.data});
+        const res = await Utility.TutorGetTutors().then(res=>{setTutors(res.data); return res.data;});
         
         //getting courses for tutors
         res.forEach(async e=> {
@@ -32,17 +32,46 @@ function Dashboard(props) {
     }
     ,[]);
 
+    const handleChange = (e)=> {
+        console.log(e.target.name + "  " + e.target.value);
+        console.log(filters);
+        setFilters(prev => ({...prev,[e.target.name]:e.target.value}));
+         //filter by tutor
+         //setTutors(prev => prev.filter(e=> `${e.firstName} ${e.lastName}` === filters["tutor_name"]));
+
+        }
+
+    const filterCourse = (course,course_name= filters["course_name"], student_name = filters["student_name"])=> {
+        if(course_name?.length > 3){
+            return course.courseName.toLowerCase().includes(course_name.toLowerCase());
+        }
+
+        if(student_name?.length > 3){
+            return course.students.map(e=> e.firstName +" "+e.lastName).join(" ").toLowerCase().includes(student_name.toLowerCase());
+        }
+
+        return true;
+    }
+
+    const filterTutorCourses = (tutor_id)=>{
+        var ret = courses[tutor_id]?.filter(f=> filterCourse(f))?.length;
+        console.log(ret)
+        return ret;
+    };
+        
     const data = useLoaderData();
-    console.log("tutors: ");
-        console.log(tutors);
-        console.log("courses: ");
-        console.log(courses);
     //console.log(data); .map(f=><Card> JSON.stringify(f)</Card>{JSON.stringify(courses[e.tutorId].map(c=>c.courseId))}
     return <>
-    <div className='App-Header'></div>
+    <div className='App-Header'><h1>Hello test {data.var}</h1></div>
+    <div><input className='input-field' onChange={handleChange} placeholder="Course Name" name="course_name" value={filters["course_name"]}></input>
+        <input className='input-field' onChange={handleChange} placeholder="Tutor Name" name="tutor_name" value={filters["tutor_name"]}></input>
+        <input className='input-field' onChange={handleChange} placeholder="Student Name" name="student_name" value={filters["student_name"]}></input>
+    </div>
     <div>
-        <h1>Hello test {data.var}</h1>
-        {tutors.map(e=><Accordion key = {e.tutorId} elevation ={15} style= {{"text-align":"center"}}>
+        {tutors.filter(f=> {if(filters["tutor_name"]?.length > 3)return `${f.firstName} ${f.lastName}`.toLowerCase().includes(filters["tutor_name"].toLowerCase()); else return true;}).map(e=> 
+            { 
+                if(filterTutorCourses(e.tutorId)==0) return;
+                 return (<Accordion key = {e.tutorId} elevation ={15} style= {{"text-align":"center"}}>
             <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
         ><Typo variant='h2'>{e.firstName} {e.lastName}</Typo>
@@ -52,9 +81,15 @@ function Dashboard(props) {
         <Typo variant='p'>Click to display courses</Typo>
         </AccordionSummary>
         <AccordionDetails>
-          <Typo variant='h4'>Course_list:</Typo> {courses[e.tutorId]?.map(c=>(<Card elevation={(c.courseId % 2 == 0)?10:15}><Typo>{c.courseName} starting at {new Date(0).setUTCSeconds(c.startDate)}</Typo></Card>))}
+          <Typo variant='h4'>Course list:</Typo> {courses[e.tutorId]?.filter(f=> filterCourse(f)).map(c=>(<Card elevation={(c.courseId % 2 == 0)?10:15}><Typo>{c.courseName} starting at {c.startDate}</Typo>
+          <br/>
+          <Typo>Students</Typo>
+          <List>
+            {c.students.map(e=> <ListItem style= {{"text-align":"center"}}>{e.firstName} {e.lastName}</ListItem>)}
+          </List>
+          </Card>))}
           </AccordionDetails>
-        </Accordion>)}
+        </Accordion>)})}
     </div>
     </>
 }
